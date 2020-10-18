20661830c050581624ab35a3c3133d2440010a4c
18008028772@qq.com <18008028772@qq.com>
2020-10-18 22:04:29 +0800

    Initial commit

---
+++ css/python/my.py
@@ -0,0 +1,6 @@
+# a = 30
+# b = 10
+# c = a*b-5
+# print(c)
+# print(3>4 or 4)
+print('A'>"a")
---
+++ my.js
@@ -0,0 +1,17 @@
+var canvas = document.getElementById('canvas');
+
+var pen = canvas.getContext('2d');
+
+// pen.fillRect(0,0,50,50);
+
+// add path
+
+pen.rect(0,0,1000,100)
+
+// filling
+pen.font = '30px Arial';
+
+pen.fillText("hello world",10,50)
+// pen.fill();
+
+pen.stroke();
